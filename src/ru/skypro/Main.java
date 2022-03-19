7a3b7b5e6bf18122a9ae07e94d8f229c0176d500
Krakodil <krakodilgena79@bk.ru>
2022-03-19 22:31:02 +0300

    выполнил дз по cycles

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,38 @@
 package ru.skypro;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//Задание 1
+        int currentNum = 0;
+        while (currentNum < 10) {
+            System.out.println(++currentNum + " ");
+        }
+        System.out.println();
+
+        for (; currentNum > 0; currentNum--) {
+            System.out.println(currentNum + " ");
+        }
+        System.out.println();
+
+    //Задание 2
+    int firstFriday = 5;
+
+        for (int currentFriday = firstFriday; currentFriday <= 31; currentFriday += 7) {
+            System.out.println("Подготовьте отчет,сегодня пятница " + currentFriday);
+        }
+    //Задание 3
+    int currentYear = LocalDate.now().getYear();
+        int start = currentYear - 200;
+        int finish = currentYear + 100;
+        for (int year = start; year <= finish; year++) {
+            if (year % 79 == 0) {
+                System.out.println(year);
+            }
+        }
     }
 }
+
+
